8b1781615c20f2b35b7a7becfa5d1675a490156e
skippharaoh <caniggiathompson@gmail.com>
2022-04-04 16:26:26 -0400

    House Robber Complete

---
+++ JavaScript/198. House Robber/HouseRobber.js
@@ -0,0 +1,36 @@
+```
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+```
+// 1st Method //
+var rob = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i-2]||0, nums[i-3]||0) + nums[i];
+    }
+    return Math.max(nums[nums.length-1]||0, nums[nums.length-2]||0);
+};
+
+// 2nd Method // incomplete
+var rob = function(nums) {
+    let idx = 0
+    let total = 0
+    if (nums.length <= 2 ) {
+        return Math.max(...nums)
+    }
+    while (idx < nums.length){
+        total += nums[idx]
+        idx += 2
+    }
+    // console.log(nums[idx])
+    return total
+}
