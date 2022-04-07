039b1a3907c2ef6fb1bc4366c3664719e410a669
skippharaoh <caniggiathompson@gmail.com>
2022-04-07 10:40:23 -0400

    House Robbers 2 Complete

--- JavaScript/213. House-Robber-2/HouseRobber2.js
+++ JavaScript/213. House-Robber-2/HouseRobber2.js
@@ -11,9 +11,62 @@ Input: nums = [2,3,2]
 Output: 3
 Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
 
+Example 2:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 3
+
 ```
 
-// 1st Method //
+// 1st Method // incomplete
 var rob = function(nums) {
-    // break
+    let max = helper(0, nums.length - 1);
+    let prevMax = helper(1, nums.length - 1);
+    return Math.max(nums[0], max, prevMax);
+
+    function helper(start, end) {
+        if (start > end) return 0;
+        if (start === end) return nums[start];
+        let max = 0;
+        for (let i = start; i <= end; i++) {
+            max = Math.max(max, nums[i] + helper(i + 2, end));
+        }
+        return max;
+    }
+};
+
+// 2nd Method //
+
+function rob(nums) {
+    if (nums.length == 0)
+        return 0;
+
+    if (nums.length == 1)
+        return nums[0];
+
+    var max1 = rob_simple(nums, 0, nums.length - 2);
+    var max2 = rob_simple(nums, 1, nums.length - 1);
+
+    return Math.max(max1, max2);
+}
+
+function rob_simple(nums,start,end) {
+    var t1 = 0;
+    var t2 = 0;
+
+    for (var i = start; i <= end; i++) {
+        var temp = t1;
+        var current = nums[i];
+        t1 = Math.max(current + t2, t1);
+        t2 = temp;
+    }
+
+    return t1;
 }
